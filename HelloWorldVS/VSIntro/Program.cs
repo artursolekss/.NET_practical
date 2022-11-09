97602dab4069694187570899b6ed270f30c1f292
Arturs Olekss <arturs.olekss@gmail.com>
2022-11-09 21:02:47 +0200

    Initial commit

---
+++ HelloWorldVS/VSIntro/Program.cs
@@ -0,0 +1,9 @@
+﻿
+class VSIntro
+{
+    public static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World 432232!");
+    }
+}
