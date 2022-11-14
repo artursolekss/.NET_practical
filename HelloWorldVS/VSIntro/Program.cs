879c5dff9e174456a903806cefa990f3ad09a077
Arturs Olekss <arturs.olekss@gmail.com>
2022-11-14 20:32:22 +0200

    14/11/2022

--- HelloWorldVS/VSIntro/Program.cs
+++ HelloWorldVS/VSIntro/Program.cs
@@ -1,10 +1,36 @@
 ﻿
+
 class VSIntro
+/////Defined by AOLEKSS on 10th of November
+/////Changed on 14th of Nov
+
 {
     public static void Main(string[] args)
     {
-        // See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Hello, World2!");
-        Console.WriteLine("Hello, World3!");
+        //int intvariable = 20;
+        ////// See https://aka.ms/new-console-template for more information
+        ////Console.WriteLine("Hello, World2!");
+        ////Console.WriteLine("Hello, World3!");
+        //Console.WriteLine(intvariable);
+        //intvariable = 18;
+        //Console.WriteLine(intvariable);
+        //intvariable = 27;
+        //Console.WriteLine(intvariable);
+
+        /////Left////////////////Center///////////////////////////Right/////////
+        string name = "John";
+        uint age = 18;///unsigned integer, because the age can't be negative
+        //int age = 18;///integer
+        string strNo = "18";
+        char gender = 'M';
+        float height = 1.87F;
+        bool underaged, smoking;
+        //Console.WriteLine(name);
+        //Console.WriteLine(age);
+        //Console.WriteLine(gender);
+        //Console.WriteLine(height);
+        //Console.WriteLine(name + " " + age + " " + gender + " " + height);
+        Console.WriteLine(name + '\n' + age + '\n' + gender + '\n' + height);
+
     }
 }
---
+++ Practical1/Practical1/Program.cs
@@ -0,0 +1,50 @@
+﻿class Practical1
+{
+    static void Main(string[] args)
+    {
+
+        //int valInt = 7;
+        //double valDob = valInt;//casting done automatically, because there is no
+        ////potential data lose
+        //Console.WriteLine(valDob);
+        //valDob = 10.56;
+        //valInt = (int) valDob;//casting done manually, because there is
+        //// the potential data lose
+        //Console.WriteLine(valInt);
+        //char charVar = 'A';
+        //valInt = (int)charVar;
+        //Console.WriteLine(valInt);
+
+        //char unicodeVar = '\u0023';
+        //Console.WriteLine(unicodeVar);
+
+        //string str = "\\u0023";
+        //Console.WriteLine(str);
+        //string strAt = @"\u0023";
+        //Console.WriteLine(strAt);
+
+        //string str1 = "C:\\Users\\AOLEKSS\\Documents\\dotnetPractical\\Practical1\\Practical1";//need to use the escape characters
+        //string str2 = @"C:\Users\AOLEKSS\Documents\dotnetPractical\Practical1\Practical1";//don't need to use the escape characters
+
+
+        //Console.WriteLine("Please, enter something:");
+        //string input = Console.ReadLine();
+        //Console.WriteLine("Please, enter something again:");
+        //string input2 = Console.ReadLine();
+        //string result = input + input2;
+        //Console.WriteLine(result);
+        //Console.WriteLine("You entered : " + input);
+        //Console.WriteLine("You entered : {0}", input);
+        Console.WriteLine("Enter number");
+        string number1str = Console.ReadLine();
+        string number2str = Console.ReadLine();
+        int number1 = Int32.Parse(number1str);
+        int number2 = Int32.Parse(number2str);
+        int result = number1 + number2;
+        Console.WriteLine(result);
+
+
+
+
+    }
+}
