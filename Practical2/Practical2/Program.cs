3f8f415498e35a90fa5aebd7ab1907fa573587a5
Arturs Olekss <arturs.olekss@gmail.com>
2022-11-23 21:24:09 +0200

    23.11

--- Practical2/Practical2/Program.cs
+++ Practical2/Practical2/Program.cs
@@ -91,37 +91,81 @@
         //    Console.WriteLine("Good evening!");
         //}
 
-        int time = Int32.Parse(Console.ReadLine());
-        if (time >= 5 && time <9)
+        //int time = Int32.Parse(Console.ReadLine());
+        //if (time >= 5 && time <9)
+        //{
+        //    if (time <= 7)
+        //    {
+        //        Console.WriteLine("Good early morning!");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Good morning!");
+        //    }
+        //}
+        //else if (time >= 9 && time < 16)
+        //{
+        //    Console.WriteLine("Good afternoon!");
+        //    if (time <= 11)
+        //    {
+        //        Console.WriteLine("Good late morning!");
+        //    }
+        //    else if(time >=12 && time <= 13)
+        //    {
+        //        Console.WriteLine("Enjoy your lunch");
+        //    }
+        //}
+        //else if (time < 24 && time >= 0)
+        //{
+        //    Console.WriteLine("Good evening!");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Input is not valid");
+        //}
+
+        string val = "ACD";
+
+        //if(val == "BCD")
+        //{
+        //    Console.WriteLine("BCD");
+        //}
+        //else if(val == "AUY")
+        //{
+        //    Console.WriteLine("AUY");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Default");
+        //}
+
+        //switch (val)
+        //{
+        //    case "BCD":
+        //        Console.WriteLine("BCD");
+        //        break;
+        //    case "ACD":
+        //        Console.WriteLine("AUY");
+        //        break;
+        //    default:
+        //        Console.WriteLine("Default");
+        //        break;
+        //}
+
+        bool condition1 = true;
+        bool condition2 = false;
+
+        if (condition1)
         {
-            if (time <= 7)
-            {
-                Console.WriteLine("Good early morning!");
-            }
-            else
-            {
-                Console.WriteLine("Good morning!");
-            }
+            Console.WriteLine("Print something");
         }
-        else if (time >= 9 && time < 16)
+        else if(condition2)
         {
-            Console.WriteLine("Good afternoon!");
-            if (time <= 11)
-            {
-                Console.WriteLine("Good late morning!");
-            }
-            else if(time >=12 && time <= 13)
-            {
-                Console.WriteLine("Enjoy your lunch");
-            }
-        }
-        else if (time < 24 && time >= 0)
-        {
-            Console.WriteLine("Good evening!");
+            Console.WriteLine("Print something more");
         }
         else
         {
-            Console.WriteLine("Input is not valid");
+            Console.WriteLine("Print something else");
         }
 
     }
---
+++ Practical3/ArraysLoopsFunctions/Program.cs
@@ -0,0 +1,92 @@
+﻿class Practical3
+{
+
+    static void Main(string[] args)
+    {
+        //Console.WriteLine(args[0]);
+        //Console.WriteLine(args[1]);
+        //Console.WriteLine(args[2]);
+        //int[] arr = { 1, 2, 3 };
+        //Console.WriteLine(arr[0]);
+        //Console.WriteLine(arr[1]);
+        //Console.WriteLine(arr[2]);
+        //Console.WriteLine(arr[3]);
+
+        //int[] arr = new int[3];
+        //Console.WriteLine(arr[0]);
+        //Console.WriteLine(arr[1]);
+        //Console.WriteLine(arr[2]);
+        //arr[0] = 1;
+        //arr[1] = 2;
+        //arr[2] = 3;
+        //Console.WriteLine(arr[0]);
+        //Console.WriteLine(arr[1]);
+        //Console.WriteLine(arr[2]);
+
+        //string[] arrstr = new string[2];
+        //arrstr[0] = "Element1";
+        //arrstr[1] = "Element2";
+
+        //string[] arrstr2 = new string[2];
+        //arrstr2[0] = "Element1";
+        //arrstr2[1] = "Element2";
+
+        //bool arraysEqual = false;
+
+        //if (arrstr[0] == arrstr2[0]
+        //    && arrstr[1] == arrstr2[1])
+        //{
+        //    arraysEqual = true;
+        //}
+
+        //arrstr[2] = "Element3";
+
+        //Console.WriteLine(arraysEqual);
+
+        //Array.Equals(arrstr, arrstr2);
+        //bool compare = arrstr == arrstr2;
+        //Console.WriteLine(compare);
+        //Console.WriteLine();
+
+        //string[] arrNewElement = new string[3];
+        //arrNewElement[0] = arrstr[0];
+        //arrNewElement[1] = arrstr[1];
+        //arrNewElement[2] = "New Element";
+
+        //int arraySize = Int32.Parse(Console.ReadLine());
+        //string[] array = new string[arraySize];
+
+
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    Console.WriteLine("Hello");
+        //}
+
+        Console.WriteLine("Enter the size of the array:");
+
+        ////bool correctSize = false;
+        ///for(;!corretSize;){
+        ///if(Int32.TryParse(.....) == true){
+        ///correctSize = true;
+        ///}
+        ///}
+        int arraySize = Int32.Parse(Console.ReadLine());
+        string[] elements = new string[arraySize];
+
+        for (int i = 0; i < elements.Length; i++)
+        //for (int i = 0; i < arraySize; i++)//the same
+        {
+            Console.WriteLine("Enter the element " + i);
+            string element = Console.ReadLine();
+            elements[i] = element;
+        }
+
+        for(int i = 0; i < elements.Length; i++)
+        {
+            Console.WriteLine(elements[i]);
+        }
+
+
+    }
+
+}
