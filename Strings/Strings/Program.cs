4bdf6ffaa8f543199ed5dbf445c631cd16317292
Arturs Olekss <arturs.olekss@gmail.com>
2022-12-02 21:21:07 +0200

    02/12/2022

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,35 @@
+﻿class StringsDemo
+{
+    public static void Main(string[] args)
+    {
+        //string str = null;
+        //Console.WriteLine(str);
+
+        //int[] arr = { 1, 2, 3 };
+        //int[] arr2 = arr;//arr and arr2 both store the same array/object/reference
+
+        //arr[1] = 7;
+        //Console.WriteLine(arr2[1]);
+
+        //bool stopInput = false;
+        //string concatenation = "";
+        //Console.WriteLine("Enter STOP to finish");
+        //while (!stopInput)
+        //{
+        //    string value = Console.ReadLine();
+        //    concatenation += value;// { 'A','B','C' } + { 'D','F','H' } = { 'A','B','C','D','F','H'}
+        //    if (value == "STOP")
+        //        stopInput = true;
+        //}
+        //Console.WriteLine("You entered " + concatenation);
+
+        string str = "A;BC;DE;T";
+        string[] arraySplit = str.Split(";");
+        foreach(string str2 in arraySplit)
+            Console.WriteLine(str2);
+
+        //12 45 87 90 ==> get integer array int[] arr = {12,45,87,90}
+
+        Console.ReadLine();
+    }
+}
