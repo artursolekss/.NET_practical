a82438b5c7ff91fb356a411b46dd61042b564bc6
Arturs Olekss <arturs.olekss@gmail.com>
2022-12-07 21:29:12 +0200

    07_12

--- Alghoritms/Alghoritms/Program.cs
+++ Alghoritms/Alghoritms/Program.cs
@@ -1,4 +1,6 @@
-﻿class Algorithms
+﻿using System.Text;
+
+class Algorithms
 {
     public static void Main(string[] args)
     {
@@ -9,36 +11,71 @@
         {
             arrInt[i] = Convert.ToInt32(arrStr[i]);
         }
-        int[] arrSort = new int[arrInt.Length];
-        for (int i = 0; i < arrInt.Length; i++)
-        {
-            if (i == 0)
-            {
-                arrSort[0] = arrInt[0];
-            }
-            for (int j = 1; j < (i + 1); j++)
-            {
-                if (i == j)//don't need to shift
-                {
-                    arrSort[j] = arrInt[i];
-                }
-                else if (arrSort[j] > arrInt[i])
-                {
-                    ShiftElements(arrSort, j, i);
-                    arrSort[j] = arrInt[i];
-                }
+        //int[] arrSort = new int[arrInt.Length];
 
+        //O(n) * O(n) * O(n) = O(n^3)
+        //for (int i = 0; i < arrInt.Length; i++)//O(n)
+        //{
+        //    for (int j = 0; j < (i + 1); j++)//O(n)
+        //    {
+        //        //1 4 7 9 0 0
+        //        if (i == j)//don't need to shift
+        //        {
+        //            arrSort[j] = arrInt[i];
+        //        }
+        //        else if (arrSort[j] > arrInt[i])
+        //        {
+        //            ShiftElements(arrSort, j, i); //O(n)
+        //            arrSort[j] = arrInt[i];
+        //            break;//break inner loop
+        //        }
+
+        //    }
+        //}
+
+        // 3 4 5 7 8 9
+        // 
+
+        //O(n) + O(n) + O(n) = O(n)
+        //O(n) * O(n) = O(n^2)
+
+
+        for(int i = 0; i < arrInt.Length; i++)//O(n)
+        {
+            for(int j = 1; j < arrInt.Length - i; j++)//O(n)
+            {
+                if (arrInt[j-1] < arrInt[j])
+                {
+                    int current = arrInt[j];
+                    arrInt[j] = arrInt[j - 1];
+                    arrInt[j - 1] = current;
+                }
             }
         }
-        Console.WriteLine(String.Concat(arrSort));
 
+        Console.WriteLine(GetArrayAsString(arrInt));
+    }
+
+    public static string GetArrayAsString(int[] arr)
+    {
+        StringBuilder strBuild = new StringBuilder("");
+        bool firstElement = true;
+        foreach(int item in arr)
+        {
+            if (!firstElement)
+                strBuild.Append(" ");
+            else
+                firstElement = false;
+            strBuild.Append(item);
+        }
+        return strBuild.ToString();
     }
 
     public static void ShiftElements(int[] arr,int from,int to)
     {//1 2 3 4 _ _ _ _ --->> 1 2 _ 3 4 _ _ _
         int next = arr[from];
         arr[from] = 0;
-        for (int i = from + 1; i < to; i++)
+        for (int i = from + 1; i < to + 1; i++)
         {
             int current = next;
             next = arr[i];
