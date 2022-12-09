57bd3b1341620354763000db07d742218ec69c6c
Arturs Olekss <arturs.olekss@gmail.com>
2022-12-09 20:58:47 +0200

    09/12

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,91 @@
+﻿class OOP
+{
+    static void Main(string[] args)
+    {
+        Computer computer = new Computer();//object computer created
+        //computer = Computer.GetInstance();
+        computer.TurnOn();
+        //computer.SetPrice(300);
+        //double price = computer.GetPrice();
+        computer.Price = 300;// set called (if we try to assign the valie)
+        double price = computer.Price; // get called (if we try to get the value)
+        //computer.price = 300;  double price = computer.price;
+
+       Date date = new Date();
+       //date.Day = 12; date.Month = 10; date.Year = 2020;
+
+    }
+
+}
+
+class Date
+{
+    private int day,month, year;
+    public int Month
+    {
+        get
+        {
+            return this.month;
+        }
+        set
+        {
+            if (value < 0 || value > 12)
+                Console.WriteLine("Moth entered is not valid");
+            else
+                this.month = value;
+        }
+    }
+
+
+}
+
+class Computer
+{
+    private double price, weightKg;
+    private string color;
+    private bool on;
+
+    public double Price
+    {
+        get {
+            //any logic can be written here
+            return this.price;
+        }
+        set { 
+        //any logic can be written here    
+            this.price = value; 
+        }
+    }
+
+
+    //public static Computer GetInstance()
+    //{
+    //    return new Computer();
+    //}
+
+    public void TurnOn()
+    {
+        on = true;
+    }
+
+    //public double GetPrice()
+    //{
+    //    return this.price;
+    //}
+
+    //public void SetPrice(double price)
+    //{
+    //    this.price = price;
+    //}
+
+    public void RandomFunction()
+    {
+     
+        Computer computer = new Computer();//
+        //this keyword - refers to the current computer
+        //(object) you are working from
+
+        //computer variable will store another computer
+    }
+
+}
