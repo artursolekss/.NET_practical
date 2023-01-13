07a51e0565b9a0f387f664fc0c4a21c9c1ee0c23
Arturs Olekss <arturs.olekss@gmail.com>
2023-01-13 19:56:15 +0200

    13/01 (part 1)

---
+++ DatabasesExamples/DatabasesExamples/DatabasesExamples/Program.cs
@@ -0,0 +1,39 @@
+﻿using MySqlConnector;
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        string connectionString = "server=localhost;uid=root;database=dotnetdemo";
+        MySqlConnection connection= new MySqlConnection(connectionString);
+        connection.Open();
+
+        MySqlCommand mySqlCommand = connection.CreateCommand();
+        mySqlCommand.CommandText = "SELECT * FROM Customer";
+        MySqlDataReader reader = mySqlCommand.ExecuteReader();
+
+        Console.WriteLine("ID;FirstName;LastName;Email;Phone");
+        //foreach(var line in reader)....
+        while (reader.Read())//when Read is called, we go to the next line
+            //if the line does not exist, then it returns false
+        {
+            Console.Write(reader.GetInt32("id"));
+            Console.Write(";");
+            Console.Write(reader.GetString("firstname"));
+            Console.Write(";");
+            Console.Write(reader.GetString("lastname"));
+            Console.Write(";");
+            Console.Write(reader.GetString("email"));
+            Console.Write(";");
+            Console.Write(reader.GetString("phone"));
+            Console.Write(";");
+            Console.Write(Environment.NewLine);
+        }
+
+        connection.Close();
+
+    }
+
+
+}
